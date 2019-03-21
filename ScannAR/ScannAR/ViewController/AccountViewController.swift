890d937dd74c2339ccfbc5ab9e1b257b59d11ff2
Ben Hakes <bhhakes@gmail.com>
2019-03-21 08:33:12 -0400

    add non-AR UI elements

---
+++ ScannAR/ScannAR/ViewController/AccountViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AccountViewController.swift
+//  ScannAR
+//
+//  Created by Benjamin Hakes on 3/21/19.
+//  Copyright © 2019 ScannAR Team. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+}
