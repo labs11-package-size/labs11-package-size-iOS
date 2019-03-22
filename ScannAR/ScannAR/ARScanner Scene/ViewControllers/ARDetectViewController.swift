56a731c5720773771b4235338c74f592d8646e55
jkaunert <44586402+jkaunert@users.noreply.github.com>
2019-03-22 13:54:41 -0500

    (feat) Implements View Controllers for detect object scene, detect object picker view, and ARScan options view. Adds assets and color objects and art.scnassets for object detect AR

---
+++ ScannAR/ScannAR/ARScanner Scene/ViewControllers/ARDetectViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ARDetectViewController.swift
+//  ScannAR
+//
+//  Created by Joshua Kaunert on 3/22/19.
+//  Copyright © 2019 ScannAR Team. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SceneKit
+import ARKit
+
+class ARDetectViewController: UIViewController, ARSCNViewDelegate {
+    
+}
---
+++ ScannAR/ScannAR/ARScanner Scene/ViewControllers/DetectObjectPickerViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetectObjectPickerViewController.swift
+//  ScannAR
+//
+//  Created by Joshua Kaunert on 3/22/19.
+//  Copyright © 2019 ScannAR Team. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetectObjectPickerViewController: UIViewController {
+    
+}
