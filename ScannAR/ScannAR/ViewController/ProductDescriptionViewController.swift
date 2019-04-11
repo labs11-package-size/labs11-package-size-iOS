0b9f55c02b8a1b03b65369454e4e1f61a9175d94
Ben Hakes <bhhakes@gmail.com>
2019-04-11 12:14:15 -0500

    UI updated to Product detail UI, added Tap Gestures where appropriate

---
+++ ScannAR/ScannAR/ViewController/ProductDescriptionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProductDescriptionViewController.swift
+//  ScannAR
+//
+//  Created by Benjamin Hakes on 4/11/19.
+//  Copyright © 2019 ScannAR Team. All rights reserved.
+//
+
+import UIKit
+
+class ProductDescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
