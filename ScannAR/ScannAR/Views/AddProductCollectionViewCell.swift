923ebab174821c0ae961a995ee858239bf11bb5d
Ben Hakes <bhhakes@gmail.com>
2019-04-15 19:33:58 -0500

    update UI for package-preview

--- ScannAR/ScannAR/Views/AddProductCollectionViewCell.swift
+++ ScannAR/ScannAR/Views/AddProductCollectionViewCell.swift
@@ -13,6 +13,10 @@ class AddProductCollectionViewCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        packageImageView.setImageColor(color: UIColor(named: "appARKADarkBlue")!)
+        plusButtonImageView.setImageColor(color: UIColor(named: "appARKADarkBlue")!)
     }
-
+    @IBOutlet weak var packageImageView: UIImageView!
+    @IBOutlet weak var plusButtonImageView: UIImageView!
+    
 }
