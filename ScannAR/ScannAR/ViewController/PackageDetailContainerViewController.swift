c9ae1ead44b358f879e70c4372ca74764ece0421
Ben Hakes <bhhakes@gmail.com>
2019-04-22 18:35:09 -0500

    (fix) new user flow directly from packages cell

---
+++ ScannAR/ScannAR/ViewController/PackageDetailContainerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PackageDetailContainerViewController.swift
+//  ScannAR
+//
+//  Created by Benjamin Hakes on 4/22/19.
+//  Copyright © 2019 ScannAR Team. All rights reserved.
+//
+
+import UIKit
+
+class PackageDetailContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
